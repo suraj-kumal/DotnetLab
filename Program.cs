48cbbc02bacc75a507630760b083f043dcdbeee2
zerocool <89600461+suraj-kumal@users.noreply.github.com>
2024-03-18 22:02:16 +0545

    c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SurajLabWork{
+    internal class Program{
+        public static void Main()
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
